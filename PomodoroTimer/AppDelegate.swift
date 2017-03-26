519e183d711ec5882d866388b4d3730830cde637
Mijo Gracanin <mgracanin@rhapsody.com>
2017-03-26 12:58:12 +0200

    Initial Commit

---
+++ PomodoroTimer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PomodoroTimer
+//
+//  Created by Mijo Gracanin on 26/03/2017.
+//  Copyright © 2017 MijoCoder. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
