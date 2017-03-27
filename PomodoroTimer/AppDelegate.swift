6e2d49fbf4250c04421eff05e68052843635c139
Mijo Gracanin <mgracanin@rhapsody.com>
2017-03-27 20:09:32 +0200

    Create simple pomodoro timer project

--- PomodoroTimer/AppDelegate.swift
+++ PomodoroTimer/AppDelegate.swift
@@ -10,18 +10,42 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    @IBOutlet weak var window: NSWindow!
-
+    
+    private var timer: Timer?
+    private let statusItem = NSStatusBar.system().statusItem(withLength: 42)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        statusItem.action = #selector(terminate)
+        
+        let startDate = Date()
+        let pomodoroInterval: Double = 25 * 60
+        
+        self.statusItem.title = formatTime(seconds: pomodoroInterval)
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block: { [weak self] timer in
+            guard let `self` = self else { return }
+            let remainingTime = pomodoroInterval - Date().timeIntervalSince(startDate)
+            
+            if remainingTime < 0 {
+                timer.invalidate()
+                let notification = NSUserNotification()
+                notification.title = "Pomodoro!"
+                notification.soundName = "Blow"
+                NSUserNotificationCenter.default.deliver(notification)
+            }
+            else {
+                self.statusItem.title = self.formatTime(seconds: remainingTime)
+            }
+        })
+    }
+    
+    func formatTime(seconds: TimeInterval) -> String {
+        let minutes = Int(seconds / 60)
+        let remainingSeconds = Int(seconds) % 60
+        return String(format:"%02d:%02d", minutes, remainingSeconds)
     }
 
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    func terminate() {
+        NSApp.terminate(self)
     }
-
-
 }
-
