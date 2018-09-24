4376938c25f4ebf300692a2455c9a323a56eb010
mgracanin <mijo.gracanin@gmail.com>
2018-09-24 09:48:24 +0200

    Add menu with work and rest timer

--- PomodoroTimer/AppDelegate.swift
+++ PomodoroTimer/AppDelegate.swift
@@ -11,41 +11,11 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
-    private var timer: Timer?
-    private let statusItem = NSStatusBar.system().statusItem(withLength: 42)
-
+    var pomodoroTimer: PomodoroTimer?
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        statusItem.action = #selector(terminate)
-        
-        let startDate = Date()
-        let pomodoroInterval: Double = 25 * 60
-        
-        self.statusItem.title = formatTime(seconds: pomodoroInterval)
-        
-        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block: { [weak self] timer in
-            guard let `self` = self else { return }
-            let remainingTime = pomodoroInterval - Date().timeIntervalSince(startDate)
-            
-            if remainingTime < 0 {
-                timer.invalidate()
-                let notification = NSUserNotification()
-                notification.title = "Pomodoro!"
-                notification.soundName = "Blow"
-                NSUserNotificationCenter.default.deliver(notification)
-            }
-            else {
-                self.statusItem.title = self.formatTime(seconds: remainingTime)
-            }
-        })
+        pomodoroTimer = PomodoroTimer()
     }
     
-    func formatTime(seconds: TimeInterval) -> String {
-        let minutes = Int(seconds / 60)
-        let remainingSeconds = Int(seconds) % 60
-        return String(format:"%02d:%02d", minutes, remainingSeconds)
-    }
 
-    func terminate() {
-        NSApp.terminate(self)
-    }
 }
---
+++ PomodoroTimer/Classes/PomodoroTimer.swift
@@ -0,0 +1,111 @@
+//
+//  PomodoroTimer.swift
+//  PomodoroTimer
+//
+//  Created by Mijo Gracanin on 22/09/2018.
+//  Copyright © 2018 MijoCoder. All rights reserved.
+//
+
+import Cocoa
+
+
+class PomodoroTimer {
+    
+    private var timer: Timer?
+    private let statusItem: NSStatusItem
+    private let workInterval: TimeInterval = 25 * 60
+    private let restInterval: TimeInterval = 5 * 60
+    private var selectedInterval: TimeInterval = 0
+    private var startDate = Date()
+    private var pomodoroCount = 0
+    private var notificationShown = false
+    
+    init() {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength) // 42
+        statusItem.button?.title = "Pomodoro"
+        statusItem.target = self
+        
+        setupMenu()
+    }
+}
+
+// MARK: - Private
+private extension PomodoroTimer {
+    
+    private func setupMenu() {
+        let menu = NSMenu(title: "Pomodoro")
+        menu.addItem(withTitle: "Work", action: #selector(startWorkTimer), keyEquivalent: "").target = self
+        menu.addItem(withTitle: "Rest", action: #selector(startRestTimer), keyEquivalent: "").target = self
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(withTitle: "Quit", action: #selector(terminate), keyEquivalent: "").target = self
+        statusItem.menu = menu
+    }
+    
+    @objc private func startWorkTimer() {
+        startTimer(timeInterval: workInterval, onTimeChange: { [weak self] timer in
+            self?.update(workTimer: timer)
+        })
+    }
+    
+    @objc private func startRestTimer() {
+        startTimer(timeInterval: restInterval, onTimeChange: { [weak self] timer in
+            self?.update(restTimer: timer)
+        })
+    }
+    
+    @objc private func terminate() {
+        timer?.invalidate()
+        NSApp.terminate(self)
+    }
+    
+    private func startTimer(timeInterval: TimeInterval, onTimeChange: @escaping (Timer) -> Void) {
+        selectedInterval = timeInterval
+        startDate = Date()
+        timer?.invalidate()
+        notificationShown = false
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5,
+                                     repeats: true,
+                                     block: onTimeChange)
+    }
+    
+    private func update(workTimer: Timer) {
+        let elapsedTime = Date().timeIntervalSince(startDate)
+        let remainingTime = selectedInterval - elapsedTime
+        
+        if remainingTime < 0 {
+            workTimer.invalidate()
+            pomodoroCount += 1
+            showNotification(title: "++Pomodoro")
+            statusItem.button?.title =  "Pomodoro count: \(pomodoroCount)"
+        }
+        else {
+            statusItem.button?.title = PomodoroTimer.formatTime(seconds: remainingTime)
+        }
+    }
+    
+    private func update(restTimer: Timer) {
+        let elapsedTime = Date().timeIntervalSince(startDate)
+        let remainingTime = selectedInterval - elapsedTime
+        
+        if remainingTime < 0 && !notificationShown {
+            showNotification(title: "Rest is over!")
+        }
+
+        statusItem.button?.title = PomodoroTimer.formatTime(seconds: remainingTime)
+    }
+    
+    private func showNotification(title: String) {
+        let notification = NSUserNotification()
+        notification.title = title
+        notification.soundName = "Blow"
+        NSUserNotificationCenter.default.deliver(notification)
+        notificationShown = true
+    }
+    
+    private static func formatTime(seconds: TimeInterval) -> String {
+        let minutes = Int(seconds / 60)
+        let remainingSeconds = Int(seconds) % 60
+        return String(format:"%02d:%02d", minutes, remainingSeconds)
+    }
+}
